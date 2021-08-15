d45f4de1a3acea3676d08cb6eebc26900c54473c
RaisanJMR <raisanjmr@protonmail.com>
2021-08-15 20:58:34 +0530

    added latest product reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -5,6 +5,7 @@ import {
 } from '../constants/productConstants'
 
 export const productListReducer = (state = { products: [] }, action) => {
+ 
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] }
