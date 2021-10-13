5734e542e1d691c08d782540b8a449b5d48e901d
RaisanJMR <raisanjmr@protonmail.com>
2021-10-13 10:34:44 +0530

    added create product functionality. working on product image upload

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -12,6 +12,10 @@ import {
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_RESET,
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_FAIL,
+  PRODUCT_UPDATE_RESET,
 } from '../constants/productConstants'
 
 // PRODUCT DETAILS REDUCER FOR HOME SCREEN
@@ -69,7 +73,23 @@ export const productCreateReducer = (state = {}, action) => {
     case PRODUCT_CREATE_FAIL:
       return { loading: false, error: action.payload }
     case PRODUCT_CREATE_RESET:
-      return {}  
+      return {}
+    default:
+      return state
+  }
+}
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+  // console.log(state)
+  switch (action.type) {
+    case PRODUCT_UPDATE_REQUEST:
+      return { loading: true }
+    case PRODUCT_UPDATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload }
+    case PRODUCT_UPDATE_FAIL:
+      return { loading: false, error: action.payload }
+    case PRODUCT_UPDATE_RESET:
+      return { product: {} }
     default:
       return state
   }
