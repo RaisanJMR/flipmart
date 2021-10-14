4ccf328fd12c9f62554ea3aa9a763e3933a16fe3
RaisanJMR <raisanjmr@protonmail.com>
2021-10-14 10:46:15 +0530

    product review end point added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
@@ -16,6 +17,10 @@ connectDB()
 const app = express()
 app.use(express.json())
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.get('/', (req, res) => {
   res.send('api running..')
 })
@@ -28,7 +33,7 @@ app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname,'/uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // NOT FOUND ERROR HANDLER
 app.use(notFound)
